e4371b7aae80c0fc2c45837b9fd49e8fc08184d8
Nico Rehwaldt <git_nikku@nixis.de>
2014-02-05 12:01:59 +0100

    chore(build/windows): use system temp folder during build

--- distro/installer/windows/build-installers.bat
+++ distro/installer/windows/build-installers.bat
@@ -2,20 +2,23 @@
 set PRODUCTS_DIR=..\..\..\org.camunda.bpm.modeler.standalone.product\target\products
 set WIN32_PRODUCTS_DIR=%PRODUCTS_DIR%\org.camunda.bpm.modeler.standalone.product\win32\win32
 
-set LOCAL_PRODUCTS_DIR=temp
+set LOCAL_PRODUCTS_DIR=%TMP%\mdist
 set LOCAL_INSTALLER_DIR=installer
 
 if NOT "%1"=="" set D_VERSION=/DVERSION=%1
 
-if "%NSIS_HOME%"=="" GOTO :Error_No_NSIS
+if "%NSIS_HOME%"=="" GOTO Error_No_NSIS
 
 echo Cleanup old files...
-del %LOCAL_INSTALLER_DIR%\*.exe
+del %LOCAL_INSTALLER_DIR%\*.exe >NUL 2>NUL
+if exist %LOCAL_PRODUCTS_DIR% rmdir /S /Q %LOCAL_PRODUCTS_DIR%
 
 echo Create local copy of install files...
 robocopy %WIN32_PRODUCTS_DIR% %LOCAL_PRODUCTS_DIR% /MIR >NUL
 
 echo Creating 32bit installer...
+echo.
+
 "%NSIS_HOME%\makensis" /DSRC_DIR=%LOCAL_PRODUCTS_DIR%\x86\modeler %D_VERSION% camundaModeler.nsi 2>&1
 if ERRORLEVEL 1 goto Error_Installer_Failed else (echo Done.)
 
@@ -25,8 +28,11 @@ echo Creating 64bit installer...
 "%NSIS_HOME%\makensis" /DSRC_DIR=%LOCAL_PRODUCTS_DIR%\x86_64\modeler %D_VERSION% /DMODELER_64 camundaModeler.nsi 2>&1
 if ERRORLEVEL 1 goto Error_Installer_Failed else (echo Done.)
 
+echo.
+echo Cleanup temp files...
+rmdir /S /Q %LOCAL_PRODUCTS_DIR%
 
-GOTO End
+goto End
 
 :Error_Installer_Failed
 echo Failed to create installer.
@@ -36,4 +42,10 @@ exit 1
 echo The environment variable NSIS_HOME is not defined. Make sure you have NSIS installed.
 exit 1
 
+
 :End
+
+echo.
+
+echo Installers saved to installer/ directory.
+echo Done.
